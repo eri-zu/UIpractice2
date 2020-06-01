8b02cf2b198dc75a4c1e2452fbee0f0e152f01cb
Eri <izutsu812@gmail.com>
2020-06-01 16:37:49 +0900

    scroll / 2

---
+++ scroll/2/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+{
+
+	const btn = document.getElementById('conversionBtnFixed');
+	const wrapper = document.getElementById('wrapper');
+	let scrollArea;
+	let scrollY;
+
+	//プロパティはwindowじゃないので注意
+	wrapper.addEventListener('scroll', fadeIn);
+
+	function fadeIn () {
+
+		scrollArea = wrapper;
+		scrollY = scrollArea.scrollTop;
+		// プロパティがwindowじゃないので、scrollTopとすること
+		// pageYOffsetやscrollYはだめ
+
+		console.log(scrollY);
+
+		if(scrollY > 800) {
+			btn.classList.add('active');
+		} else {
+			btn.classList.remove('active');
+		}
+
+	}
+
+}
