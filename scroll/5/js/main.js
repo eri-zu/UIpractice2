6c64100c1e7652af0bebb1f9c8881fca0785c665
Eri <izutsu812@gmail.com>
2020-06-03 14:54:53 +0900

    scroll/5

---
+++ scroll/5/js/main.js
@@ -0,0 +1,39 @@
+'use strict';
+{
+	// 目標：スムーススクロールでリンク先に移動する、移動スピードは調整できるようにする
+
+	const btns = document.querySelectorAll('.menu-item');
+	const duration = 5;
+
+	btns.forEach(btn => {
+
+		btn.addEventListener('click', e => {
+
+			e.preventDefault;
+
+			const clickedItem = e.target;
+			const targetHref = clickedItem.getAttribute('href');
+			const destination = document.querySelector(targetHref);
+
+			const offsetTop = destination.getBoundingClientRect().top;
+
+			console.log(clickedItem);
+			console.log(targetHref);
+			console.log(destination);
+			console.log(offsetTop);
+
+			TweenMax.to(window, duration, {
+				y: offsetTop
+			});
+
+			// function goToLink () {
+
+			// }
+
+			// goToLink();
+
+		});
+		
+	});
+
+}
