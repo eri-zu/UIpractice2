d03c74016aa0c46c5c518102afd804f258bd7653
Eri <izutsu812@gmail.com>
2020-06-08 15:08:16 +0900

    slider / 1

---
+++ slider/1/js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// 基本のスライダー
+// アニメーションについて↓
+// effectは切り替わる時、明るめのグレースケールで
+// 切り替わった後は、鮮やかになる（グレースケールから徐々に色が入っていくイメージ）
+
+{
+	const slider = document.querySelector('.slider');
+	const imgs = document.querySelectorAll('.slider-item');
+	const indicators = document.querySelectorAll('.indicator div')
+	let len = imgs.length;
+	let current = 0;
+	let timer;
+
+	// インデックス番号に応じて表示するスライドを決定
+	function goToSlide(index) {
+		current = index; 
+		updatePhoto();
+		updateIndicator();
+	}
+
+	function updatePhoto() {
+		imgs.forEach(img => {
+			img.classList.remove('active');
+			img.classList.remove('active-effect');
+		});
+
+		imgs[current].classList.add('active');
+		imgs[current].classList.add('active-effect');
+	}
+
+	function updateIndicator () {
+		indicators.forEach(indicator => {
+			indicator.classList.remove('active');
+		});
+
+		indicators[current].classList.add('active');
+	}
+
+	// mvをクリックすると次のスライドへうつる
+	slider.addEventListener('click', () => {
+		const nextIndex = (current + 1) % len;
+		goToSlide(nextIndex);
+	});
+
+	// オートプレイ
+	function autoPlay () {
+		timer = setInterval(() => {
+			const nextIndex = (current + 1) % len;
+			goToSlide(nextIndex);
+		}, 7000);
+	}
+
+	goToSlide(current);
+	autoPlay();
+
+}
