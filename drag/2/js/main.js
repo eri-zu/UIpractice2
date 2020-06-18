4f7fd38c59c4d7dd0183294ab7a971a323dc1279
Eri <izutsu812@gmail.com>
2020-06-18 11:20:29 +0900

    no message

---
+++ drag/2/js/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// 目標：ドラッグ量を取得
+
+{
+	const amount = document.querySelector('.amount');
+	const sliderInner = document.querySelector('.slider-inner');
+	let isDrag = false;
+	let startPosX = 0;
+	let currentPosX = 0;
+	let dist = 0;
+
+	document.addEventListener('mousedown', mouseDownHandler);
+	document.addEventListener('mousemove', mouseMoveHandler);
+	document.addEventListener('mouseup', mouseUpHandler);
+
+	function mouseDownHandler (e) {
+		isDrag = true;
+		startPosX = e.pageX;
+		sliderInner.classList.add('active');
+	}
+
+	function mouseMoveHandler (e) {
+		currentPosX = e.pageX;
+		if(isDrag) {
+			amount.innerHTML = currentPosX - startPosX;
+			dist = currentPosX - startPosX;
+			console.log(dist);
+
+			TweenMax.to(sliderInner, 0.1, {
+				// ease: Power4.easeInOut,
+				x: dist
+			});
+			
+		} 
+	}
+
+	function mouseUpHandler (e) {
+		isDrag = false;
+		sliderInner.classList.remove('active');
+	}
+	
+
+}
