4723dfd2f59da267bf671d9561b057b220262076
Eri <izutsu812@gmail.com>
2020-06-23 17:50:48 +0900

    no message

--- loading/5/OOP/js/Util/Images.js
+++ loading/5/OOP/js/Util/Images.js
@@ -4,7 +4,7 @@ export default class Images extends EventTarget {
 		super ();
 		// DOM
 		this.imgs = document.querySelectorAll('img');
-		this.len = this.imgs.length;
+		// this.len = this.imgs.length;
 	}
 
 	setSrc() {
